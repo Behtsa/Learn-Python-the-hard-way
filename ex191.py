48de9ba00809d3a9cbcd527764464806648e142e
behtsa <monze_gdbm@hotmail.com>
2017-05-10 21:26:50 -0500

    Exercise 19.1 with .format()

--- ex191.py
+++ ex191.py
@@ -1,1 +1,17 @@
-ex191.py
+def my_friends_ages(name, age):
+	print "Name: {} and age: {}".format(name, age)
+
+print "We can start with the ones closer to me"
+first_friend_name = "Gustavo"
+first_friend_age = 24
+
+second_friend_name = "Naham"
+second_friend_age = 23
+
+third_friend_name = "Selene"
+third_friend_age = 24
+
+my_friends_ages(first_friend_name, first_friend_age)
+my_friends_ages(second_friend_name, second_friend_age)
+my_friends_ages(third_friend_name, third_friend_age)
+
